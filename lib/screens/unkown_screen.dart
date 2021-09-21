06d7f9dc9eca78bde6e12d52e4a0af8908a5f219
gyvm <jolcde75+github@gmail.com>
2021-09-22 02:03:04 +0900

    UnknownScreenの修正

--- lib/screens/unkown_screen.dart
+++ lib/screens/unkown_screen.dart
@@ -12,41 +12,57 @@ class UnknownScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: double.infinity,
-      width: double.infinity,
-      alignment: Alignment.center,
-      color: HexColor('#EFE2DB'),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                '404',
-                style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: HexColor('#8A5C46')),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: HexColor('#EFE2DB'),
+        appBar: AppBar(
+            elevation: 0,
+            backgroundColor: Colors.transparent,
+            title: Text(
+              '👋🐑',
+              style: TextStyle(
+                fontSize: 32,
               ),
             ),
-            Text(
-              '🐺',
-              style: TextStyle(fontSize: 80, fontWeight: FontWeight.bold),
+            centerTitle: false,
+            automaticallyImplyLeading: false),
+        body: Container(
+          height: double.infinity,
+          width: double.infinity,
+          alignment: Alignment.center,
+          color: HexColor('#EFE2DB'),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    '404',
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: HexColor('#8A5C46')),
+                  ),
+                ),
+                Text(
+                  '🐺',
+                  style: TextStyle(fontSize: 80, fontWeight: FontWeight.bold),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    'このページは存在しません。。',
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: HexColor('#8A5C46')),
+                  ),
+                ),
+                Container(height: 200)
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                'このページは存在しません。。',
-                style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: HexColor('#8A5C46')),
-              ),
-            ),
-            Container(height: 200)
-          ],
+          ),
         ),
       ),
     );
