f5cb4062251ef5fe8b8b540c92eb6f63a0d0f661
gyvm <jolcde75+github@gmail.com>
2021-10-04 01:10:01 +0900

    ディレクトリ名の変更(details_screen/)

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -5,15 +5,15 @@ import 'dart:html';
 
 // Flutter imports:
 import 'package:flutter/material.dart';
-import 'package:hi_tsujisan_frontend/widgets/event_screen/comments.dart';
+import 'package:hi_tsujisan_frontend/widgets/details_screen/comments.dart';
 
 // Package imports:
 import 'package:http/http.dart' as http;
 import 'package:http_retry/http_retry.dart';
 
 // Project imports:
-import 'package:hi_tsujisan_frontend/widgets/event_screen/event_info.dart';
-import 'package:hi_tsujisan_frontend/widgets/event_screen/possible_dates_table.dart';
+import 'package:hi_tsujisan_frontend/widgets/details_screen/event_info.dart';
+import 'package:hi_tsujisan_frontend/widgets/details_screen/possible_dates_table.dart';
 import '../common/h2text.dart';
 import '../common/hexcolor.dart';
 import '../page_state.dart';
